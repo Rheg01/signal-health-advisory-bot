c2ddf8a6101bb348000fe10ae3a6b645c305395f
JOHN RHEG REGAYA <143176054+Rheg01@users.noreply.github.com>
2026-03-19 17:53:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Updating system..."
 apt-get update
-apt-get install -y curl wget unzip default-jre-headless
+apt-get install -y curl wget unzip openjdk-21-jre-headless
 
 echo "Downloading signal-cli..."
 curl -L https://github.com/AsamK/signal-cli/releases/download/v0.13.3/signal-cli-0.13.3.tar.gz -o signal-cli.tar.gz
