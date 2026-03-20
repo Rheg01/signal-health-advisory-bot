875a2979796a0463cd15490a8801817cd3a30591
JOHN RHEG REGAYA <143176054+Rheg01@users.noreply.github.com>
2026-03-20 12:58:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,11 @@ set -e
 
 echo "Updating system..."
 apt-get update
-apt-get install -y curl wget unzip default-jre-headless
+apt-get install -y apt-utils curl wget unzip default-jre-headless
 
-echo "Downloading signal-cli (Java 17 compatible)..."
+echo "Downloading signal-cli..."
 curl -L https://github.com/AsamK/signal-cli/releases/download/v0.12.6/signal-cli-0.12.6.tar.gz -o signal-cli.tar.gz
 
-echo "Extracting..."
 tar -xzf signal-cli.tar.gz
 mv signal-cli-0.12.6 signal-cli
 
